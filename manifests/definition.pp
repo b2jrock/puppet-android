85db18c39f3d01a431ba1dbebbd52da4a14b17c6
Brian Johnson <brian.johnson@gree.net>
2014-05-30 12:31:40 -0700

    Deal with multiple licenses

--- manifests/definition.pp
+++ manifests/definition.pp
@@ -20,7 +20,7 @@ define android::definition(
     }
   } else {
     exec { "android-update-${filter}":
-      command  => "echo y|${android::config::sdk_dir}/bin/android update sdk --all --no-ui --filter ${filter}",
+      command  => "(while [ 1 ]; do sleep 1; echo y; done)|${android::config::sdk_dir}/bin/android update sdk --all --no-ui --filter ${filter}",
       provider => 'shell',
       timeout  => 0,
       creates  => $dest,
